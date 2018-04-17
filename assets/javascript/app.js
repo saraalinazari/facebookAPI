ca3d94a878c75699e4a20d83b1ec8ada0e0d8e90
saraalinazari <sara.alinazari@gmail.com>
2018-04-16 18:01:33 -0700

    first

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+ 
+ $(document).ready(function(){
+
  window.fbAsyncInit = function() {
     FB.init({
       appId            : '258507278047521',
@@ -15,13 +18,33 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
-   FB.ui({
-    method: 'share_open_graph',
-    action_type: 'og.likes',
-    action_properties: JSON.stringify({
-      object:'https://developers.facebook.com/docs/',
-    })
-  }, function(response){
-    // Debug response (optional)
-    console.log(response);
-  });
+//    FB.ui({
+//     method: 'share_open_graph',
+//     action_type: 'og.likes',
+//     action_properties: JSON.stringify({
+//       object:'https://developers.facebook.com/docs/',
+//     })
+//   }, function(response){
+//     // Debug response (optional)
+//     console.log(response);
+//   });
+FB.login(function(response)
+{
+if(response.session)
+{
+    if(response.perms)
+    {
+        alert('yippee');
+    }
+    else
+    {
+        alert('oh no');
+    }
+}
+else
+{
+    alert('login silly');
+}
+}, {perms:'email,user_birthday'});
+
+ });
