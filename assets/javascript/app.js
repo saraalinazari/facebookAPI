6c57c4ec9049c6c004b72a88f55e9b48dabcbe9b
saraalinazari <sara.alinazari@gmail.com>
2018-04-16 18:48:15 -0700

    first

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,33 +11,33 @@
   };
 
   
-//    FB.ui({
-//     method: 'share_open_graph',
-//     action_type: 'og.likes',
-//     action_properties: JSON.stringify({
-//       object:'https://developers.facebook.com/docs/',
-//     })
-//   }, function(response){
-//     // Debug response (optional)
-//     console.log(response);
-//   });
-FB.login(function(response)
-{
-if(response.session)
-{
-    if(response.perms)
-    {
-        alert('yippee');
-    }
-    else
-    {
-        alert('oh no');
-    }
-}
-else
-{
-    alert('login silly');
-}
-}, {perms:'email,user_birthday'});
+   FB.ui({
+    method: 'share_open_graph',
+    action_type: 'og.likes',
+    action_properties: JSON.stringify({
+      object:'https://developers.facebook.com/docs/',
+    })
+  }, function(response){
+    // Debug response (optional)
+    console.log(response);
+  });
+// FB.login(function(response)
+// {
+// if(response.session)
+// {
+//     if(response.perms)
+//     {
+//         alert('yippee');
+//     }
+//     else
+//     {
+//         alert('oh no');
+//     }
+// }
+// else
+// {
+//     alert('login silly');
+// }
+// }, {perms:'email,user_birthday'});
 
  });
