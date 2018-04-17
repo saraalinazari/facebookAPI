13799876065818a892c3404b86f6394aea798a0a
saraalinazari <sara.alinazari@gmail.com>
2018-04-16 17:35:25 -0700

    first

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=258507278047521";//"https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
