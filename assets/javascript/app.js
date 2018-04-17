ed7e950041fee59533106eae2c277743e21c9d4a
saraalinazari <sara.alinazari@gmail.com>
2018-04-16 18:46:00 -0700

    first

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,14 +10,7 @@
     });
   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=258507278047521";//"https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
+  
 //    FB.ui({
 //     method: 'share_open_graph',
 //     action_type: 'og.likes',
